9851b7a32c3c5c5358b6aa491fef6e0517e1dab4
1020phug <1020phug@gmail.com>
2025-06-13 04:37:00 +0700

    Hotfix sidebar in tweets

--- src/app/tweets/page.tsx
+++ src/app/tweets/page.tsx
@@ -1,17 +1,26 @@
+"use client";
 import NewTweet from "@/layouts/homepage/new-tweet";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ForYou from "@/layouts/homepage/for-you";
 import Following from "@/layouts/homepage/following";
+import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 
 const HomePage = () => {
+	const { isMobile } = useSidebar();
 	return (
 		<div className="bg-background">
 			<Tabs defaultValue="for-you" className="w-full">
-				<TabsList className="w-full mx-auto flex items-center justify-center bg-transparent border-b border-border rounded-none h-auto pt-4">
+				<TabsList className="w-full mx-auto flex items-center justify-center bg-transparent border-b border-border rounded-none h-auto pt-4 relative">
 					<TabsTrigger value="for-you">For you</TabsTrigger>
 					<TabsTrigger value="following">Following</TabsTrigger>
+					{isMobile && (
+						<div className="absolute right-4">
+							<SidebarTrigger />
+						</div>
+					)}
 				</TabsList>
+
 				<NewTweet />
 				<Separator className="my-4" />
 				<TabsContent value="for-you">
